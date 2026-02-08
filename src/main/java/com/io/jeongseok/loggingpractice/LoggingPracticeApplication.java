94e6d95147879d346fb294138f5aa5b6f786e756
jeongseok <ljs0429777@gmail.com>
2026-02-08 16:25:17 +0900

    Initial commit

---
+++ src/main/java/com/io/jeongseok/loggingpractice/LoggingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.io.jeongseok.loggingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/io/jeongseok/loggingpractice/LoggingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.jeongseok.loggingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
